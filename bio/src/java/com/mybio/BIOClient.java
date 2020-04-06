5d5a40687416e94a596ecad8ff6626c4bca23bc2
sutao <296652764@qq.com>
2020-04-06 17:01:22 +0800

    BIOTest

---
+++ bio/src/java/com/mybio/BIOClient.java
@@ -0,0 +1,47 @@
+package com.mybio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class BIOClient {
+
+    public static void main(String[] args) {
+        Socket socket = null;
+        OutputStream os = null;
+        try {
+            socket = new Socket("192.168.1.109", 8080);
+            os = socket.getOutputStream();
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println("客戶端，請輸入内容: ");
+                String count = scanner.next();
+                if ("exit".equalsIgnoreCase(count)) {
+                    break;
+                } else {
+                    os.write(count.getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != os) {
+                    os.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (null != socket) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
